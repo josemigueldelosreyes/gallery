0ac8d2d7caffb96ba9e7c817b12f22d4e61586d3
David De los reyes luque <davidrl@MacBook-Pro-de-David.local>
2024-08-12 06:25:58 +0200

    First commit

---
+++ src/app/components/proyects/proyects.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyects',
+  standalone: true,
+  imports: [],
+  templateUrl: './proyects.component.html',
+  styleUrl: './proyects.component.css'
+})
+export class ProyectsComponent {
+
+  images = [
+    '../../../assets/images/001.jpg',
+    '../../../assets/images/002.jpg',
+    '../../../assets/images/003.jpg',
+    '../../../assets/images/004.jpg',
+    '../../../assets/images/005.jpg',
+    '../../../assets/images/006.jpg',
+    '../../../assets/images/007.jpg',
+    '../../../assets/images/008.jpg',
+    '../../../assets/images/009.jpg',
+    '../../../assets/images/010.jpg',
+    '../../../assets/images/011.jpg',
+    '../../../assets/images/012.jpg',
+    '../../../assets/images/013.jpg',
+    '../../../assets/images/014.jpg',
+    '../../../assets/images/015.jpg',
+    '../../../assets/images/016.jpg',
+    '../../../assets/images/017.jpg',
+    '../../../assets/images/018.jpg',
+    '../../../assets/images/019.jpg',
+    '../../../assets/images/020.jpg',
+    '../../../assets/images/021.jpg'
+  ];
+}
